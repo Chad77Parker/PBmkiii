e391a054f568cf728c61255787e06b3db446111f
Chad77Parker <32125713+Chad77Parker@users.noreply.github.com>
2018-08-26 20:43:20 +1000

    Add files via upload

--- LogOut.php
+++ LogOut.php
@@ -1,40 +1,28 @@
 <?php
-session_start();
+require_once 'GlobalFunctions.php';
+require_once 'data/dbintegration.php';
 $_SESSION['loggedin']=false;
-include 'GlobalFunctions.php';
-echo '
-<html>
-<head>
-<title>Parker Bros Earthmoving Pty Ltd</title>';
-echo MobileDetect();
+echo '<html>
+      <head>
+      <title>Parker Bros Earthmoving Pty Ltd</title>';
+echo MobileDetect(); /*must be in html header*/
+echo '</head>
+     <body>
+
+     <img id="topbanner" src="images\pbbanner1.jpg"  border="0">
+     <div id="topbanner">
+     Parker Bros Earthmoving Pty Ltd.
+     </div>
+
+     <div id="background">&nbsp</div>';
+
+StandardMenu();
 ?>
-
-  <meta name="description" content="Earthmoving">
-  <meta name="keywords" content="earthmoving, dams, driveways, soil conservation, erosion control, water cartage, water delivery, road construction, earthworks">
-</head>
-<body>
-
-<img id="topbanner" src="images\pbbanner1.jpg" height="100%" border="0">
-<div id="topbanner">
-Parker Bros Earthmoving Pty Ltd.
-</div>
-
-
+<div id="background">&nbsp</div>
 <div id="scroller">
 <h3>You have successfully logged out.<br>Thank you.<br></h3>
 <p class="general">
-This site is under construction and is only for testing purposes no information contained herein is of any factual events or persons
-</p>
+
 </div>
-<?php
-StandardMenu();
-if ($_SESSION['loggedin'] and !checktimeout()){
-	LoggedInMenu();
-}
-?>
-
-<div id="background">&nbsp</div>
-
-
 </body>
 </html>
