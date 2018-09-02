e98f9a4c202f134c61fa0f006c90e20669fc213c
Chad77Parker <32125713+Chad77Parker@users.noreply.github.com>
2018-09-02 20:39:39 +1000

    Add files via upload

--- PrintServiceHistory.php
+++ PrintServiceHistory.php
@@ -1,38 +1,18 @@
 <?php
-//load global functions, check that user is logged in, and initialise database***********
-session_start();
-include 'GlobalFunctions.php';
-if ($_SESSION['loggedin']!=true){
-die('You are not authorised to view this page');
-}
-if (checktimeout()){
-	die('Your connection has expired please log back in.<a href="ParkerBros.php">Return Home</a>');
-}
+require_once 'GlobalFunctions.php';
+require_once 'data/dbintegration.php';
+
 echo '<html>
       <head>
-      <title>Parker Bros Earthmoving Pty Ltd</title>
-      <link href="data/standardprint.css" type="text/css" rel="stylesheet" />'; /*must be in html header*/
-?>
-  
-</head>
-<body>
+      <title>Parker Bros Earthmoving Pty Ltd</title>';
+echo '<link href="data/standardprint.css" type="text/css" rel="stylesheet" />';
+echo '</head>
+     <body>';
 
-<img id="topbanner" src="images\pbprintbanner1.jpg"  border="0">
+if (checktimeout()){die('<div id= "scroller">You are not authorised to view this page or your session has expired please log in. <a href="ParkerBros.php">Return Home</a></div>');}
 
-<?php
-//connect to database
-$link = @mysql_connect($_SESSION['host'], $_SESSION['user'], $_SESSION['pass']);
-if (!$link) {
-    die('Could not connect to MySQL server: ' . mysql_error());
-}
-$dbname = $_SESSION['datab'];
-$db_selected = mysql_select_db($dbname, $link);
-if (!$db_selected) {
-    die("Could not set $dbname: " . mysql_error());
-}
-
-
-
+echo '<div id="scroller">
+     <img id="topbanner" src="images\pbprintbanner1.jpg"  border="0">';
 
 ?>
 
@@ -47,22 +27,15 @@ if($_POST['Vehicle']=='All'){
 $query = 'Select * from parkerbros.servicehistory where Vehicle="'.$_POST['Vehicle'].'" and ServDate between "'.$StartDate.'" and "'.$EndDate.'" order by ServDate DESC;';
 }
 
-$res = mysql_query($query);
-if(!$res){
-	die(mysql_error());
-}
+$res = dbquery($query);
 $query2 = 'select * from parkerbros.Vehicles where Ind="'.$_POST['Vehicle'].'";';
-
-$res2 = mysql_query($query2);
-if(!$res2){
-	die(mysql_error());
-}
-$row2=mysql_fetch_assoc($res2);
+$res2 = dbquery($query2);
+$row2=dbfetchassoc($res2);
 
 echo '<form><table border="1"><tr><td colspan="8" class="SubHeading1">Service History for '.$row2['Make'].' '.$row2['Model'].', '.$row2['Name'].'<tr>'.
      '<td colspan="8" class="SubHeading2">From '.date('l jS \of F, Y.',strtotime($StartDate)).' to '.date('l jS \of F, Y.',strtotime($EndDate)).
      '<tr><td class="SubHeading3">Date<td class="SubHeading3">Amount<td class="SubHeading3">Units<td class="SubHeading3">Schedueled<td class="SubHeading3">Daily Checklist#<td class="SubHeading3">Item serviced<td class="SubHeading3">Work Completed  <td class="SubHeading3">Comments';
-while($row=mysql_fetch_assoc($res)){
+while($row=dbfetchassoc($res)){
      echo '<tr><td class="general">'.$row['ServDate'].'<td class="general">'.$row['ServUnitsAmount'].'<td class="general">'.$row['ServUnitsType'].'<td class="general">'.$row['SchedueledService'].'<td class="general">'.$row['DailyChecklistInd'].'<td class="general">'.$row['ServItem'].'<td class="general">'.$row['WorkCompleted'].'<td class="general">'.$row['Comments'];
 
 
@@ -80,9 +53,5 @@ echo '</table></form>
 ?>
 </p>
 </div>
-
-
-<div id="background">&nbsp</div>
-
 </body>
 </html>
