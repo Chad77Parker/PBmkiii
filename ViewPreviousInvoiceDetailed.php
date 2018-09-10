685a4def9e8b528be013a129a0ef79d026e1053a
Chad77Parker <32125713+Chad77Parker@users.noreply.github.com>
2018-09-10 17:21:03 +1000

    Add files via upload

--- ViewPreviousInvoiceDetailed.php
+++ ViewPreviousInvoiceDetailed.php
@@ -1,60 +1,33 @@
 <?php
-session_start();
-include 'GlobalFunctions.php';
-if ($_SESSION['loggedin']!=true){
-die('You are not authorised to view this page');
-}
-if (checktimeout()){
-	die('Your connection has expired please log back in.<a href="ParkerBros.php">Return Home</a>');
-}
+require_once 'GlobalFunctions.php';
+require_once 'data/dbintegration.php';
+
 echo '<html>
       <head>
       <title>Parker Bros Earthmoving Pty Ltd</title>';
-echo MobileDetect();
-?>
-  
-</head>
-<body>
+echo MobileDetect(); /*must be in html header*/
+echo '</head>
+     <body>
 
-<img id="topbanner" src="images\pbbanner1.jpg"  border="0">
-<div id="topbanner">
-Parker Bros Earthmoving Pty Ltd.
-</div>
+     <img id="topbanner" src="images\pbbanner1.jpg"  border="0">
+     <div id="topbanner">
+     Parker Bros Earthmoving Pty Ltd.
+     </div>
 
-
-<?php
-//connect to database
-$link = @mysql_connect($_SESSION['host'], $_SESSION['user'], $_SESSION['pass']);
-if (!$link) {
-    die('Could not connect to MySQL server: ' . mysql_error());
-}
-$dbname = $_SESSION['datab'];
-$db_selected = mysql_select_db($dbname, $link);
-if (!$db_selected) {
-    die("Could not set $dbname: " . mysql_error());
-}
-
-
+     <div id="background">&nbsp</div>';
 
 StandardMenu();
-if ($_SESSION['loggedin'] and !checktimeout()){
-	LoggedInMenu();
-}
+if (checktimeout()){die('<div id= "scroller">You are not authorised to view this page or your session has expired please log in. <a href="ParkerBros.php">Return Home</a></div>');}
+LoggedInMenu();
 ?>
-
-
+<div id="background">&nbsp</div>
 <div id="scroller">
 <?php
-
-
 $query='select * from Invoices where InvoiceNumber="'.$_POST['Invoice'].'";';
-$res=mysql_query($query);
-if(!$res){
-	die(mysql_error());
-}
+$res=dbquery($query);
 $firstjobflag=true;
 $queryjobs='select * from jobs where Ind in (';
-while ($row = mysql_fetch_assoc($res)){
+while ($row = dbfetchassoc($res)){
 	if ($firstjobflag==false and isset($_POST['Invoice'])){
 		$queryjobs= $queryjobs.', ';
 	}
@@ -67,23 +40,16 @@ while ($row = mysql_fetch_assoc($res)){
 	}
 }
 $queryjobs= $queryjobs.') order by ClientInd, EndDate;';
-$JobsRes=mysql_query($queryjobs);
-if(!$JobsRes){
-	die(mysql_error());
-}
-
+$JobsRes=dbquery($queryjobs);
 $InvoiceNumber=$_POST['Invoice'];
 echo '<h3>Invoice: '.$InvoiceNumber.'</h3>';
 $PrevClient=0;
 echo 	'<br><form method="post" action="PrintInvoiceDetailed.php"><table class="threequarterwidth" border="2">';
-while ($rowJ = mysql_fetch_assoc($JobsRes)){
+while ($rowJ = dbfetchassoc($JobsRes)){
 if ($PrevClient!=$rowJ['ClientInd']){
 	$query = 'select * from contacts where Ind = '.$rowJ['ClientInd'].';';
-	$ClientRes=mysql_query($query);
-	if(!$ClientRes){
-	die(mysql_error());
-	}
-	while ($rowC = mysql_fetch_assoc($ClientRes)){
+	$ClientRes=dbquery($query);
+	while ($rowC = dbfetchassoc($ClientRes)){
 	 echo 	'<tr><td class="SubHeading1" colspan="5"><br>'.$rowC['ContactTitleOrPosition'].' '.$rowC['ContactFirstName'].' '.$rowC['ContactLastName'].', '.$rowC['Company'].'</td><tr><td class="general" colspan="5">'.$rowC['PostalAddressNumber'].' '.$rowC['PostalAddressStreet'].', '.$rowC['PostalAddressSuburb'].', '.$rowC['PostalAddressState'].', '.$rowC['PostalAddressPostCode'].'</td>';
 	}
 $PrevClient=$rowJ['ClientInd'];
@@ -91,34 +57,25 @@ $PrevClient=$rowJ['ClientInd'];
 echo 	'<tr><td class="SubHeading2" colspan="5">'.$rowJ['JobDescription'].'</td>
 	<tr><td class="general" colspan="5">Comenced: '.date('j/n/Y',strtotime($rowJ['StartDate'])).'&nbsp &nbsp &nbsp &nbsp Completed: '.date('j/n/Y',strtotime($rowJ['EndDate']));
 $query='select JobInd, Vehicle, sum(HoursForVehicle) from vehiclehours where JobInd='.$rowJ['Ind'].' group by Vehicle;';
-$VehicleHoursRes=mysql_query($query);
-if(!$VehicleHoursRes){
-	die(mysql_error());
-}
+$VehicleHoursRes=dbquery($query);
 $PlantTotal=0;
-if (mysql_num_rows($VehicleHoursRes)!=0){
+if (dbnumrows($VehicleHoursRes)!=0){
 	echo 	'<tr><td class="SubHeading3">Plant Hire<td class="SubHeading3">$/hr<td class="SubHeading3">hrs<td class="SubHeading3">Sub<td class="SubHeading3">Total';
-	while ($rowVH=mysql_fetch_assoc($VehicleHoursRes)){
+	while ($rowVH=dbfetchassoc($VehicleHoursRes)){
 	 $query='select Name, Make, Model, PricePerHour from vehicles where Ind='.$rowVH['Vehicle'].';';
-	 $VehicleRes=mysql_query($query);
-	 if(!$VehicleRes){
-	 die(mysql_error());
-	 }
-	 $rowV=mysql_fetch_assoc($VehicleRes);
+	 $VehicleRes=dbquery($query);
+	 $rowV=dbfetchassoc($VehicleRes);
 	 echo 	'<tr><td class="general">'.$rowV['Name'].', '.$rowV['Make'].' '.$rowV['Model'].'<td class="general">$'.$rowV['PricePerHour'].'<td class="general">'.$rowVH['sum(HoursForVehicle)'].'<td class="general">$'.$rowV['PricePerHour']*$rowVH['sum(HoursForVehicle)'].'<td class="general">';
   $PlantTotal=$PlantTotal+($rowV['PricePerHour']*$rowVH['sum(HoursForVehicle)']);
 	}
 	echo 	'<tr><td class="Totals" colspan="4">Plant Hire Total<td class="Totals">$'.$PlantTotal;
 }
 $query='select Material, max(MaterialPrice), sum(MaterialQuantity), JobInd from materialsused where JobInd='.$rowJ['Ind'].'	group by Material;';
-$MaterialsRes=mysql_query($query);
-if(!$MaterialsRes){
-	die(mysql_error());
-}
+$MaterialsRes=dbquery($query);
 $MaterialTotal=0;
-if (mysql_num_rows($MaterialsRes)!=0){
+if (dbnumrows($MaterialsRes)!=0){
 	echo 	'<tr><td class="SubHeading3">Materials<td class="SubHeading3">$/Unit<td class="SubHeading3">Qty<td class="SubHeading3">Sub<td class="SubHeading3">Total';
-	while ($rowM=mysql_fetch_assoc($MaterialsRes)){
+	while ($rowM=dbfetchassoc($MaterialsRes)){
 		echo 	'<tr><td class="general">'.$rowM['Material'].'<td class="general">$'.$rowM['max(MaterialPrice)'].'<td class="general">'.$rowM['sum(MaterialQuantity)'].'<td class="general">$'.$rowM['max(MaterialPrice)']*$rowM['sum(MaterialQuantity)'].'<td class="general">';
 		$MaterialTotal+=($rowM['max(MaterialPrice)']*$rowM['sum(MaterialQuantity)']);
 	
@@ -132,31 +89,22 @@ echo '<tr><td class="MasterTotals" colspan="4">TOTAL<td class="MasterTotals">$'.
 // start of details
 echo '<tr><td class="SubHeading2" colspan="5">Details</td>';
 $query='select JobInd, Vehicle, HoursForVehicle, OperationDate from vehiclehours where JobInd='.$rowJ['Ind'].' order by OperationDate;';
-$VehicleHoursRes=mysql_query($query);
-if(!$VehicleHoursRes){
-	die(mysql_error());
-}
-if (mysql_num_rows($VehicleHoursRes)!=0){
+$VehicleHoursRes=dbquery($query);
+if (dbnumrows($VehicleHoursRes)!=0){
 	echo 	'<tr><td class="SubHeading3">Plant Hire<td class="SubHeading3">Date<td class="SubHeading3" colspan="3">hrs';
-	while ($rowVH=mysql_fetch_assoc($VehicleHoursRes)){
+	while ($rowVH=dbfetchassoc($VehicleHoursRes)){
 	 $query='select Name, Make, Model, PricePerHour from vehicles where Ind='.$rowVH['Vehicle'].';';
-	 $VehicleRes=mysql_query($query);
-	 if(!$VehicleRes){
-	 die(mysql_error());
-	 }
-	 $rowV=mysql_fetch_assoc($VehicleRes);
+	 $VehicleRes=dbquery($query);
+	 $rowV=dbfetchassoc($VehicleRes);
 	 echo 	'<tr><td class="general">'.$rowV['Name'].', '.$rowV['Make'].' '.$rowV['Model'].'<td class="general">'.date('j/n/Y',strtotime($rowVH['OperationDate'])).'<td class="general" colspan="3">'.$rowVH['HoursForVehicle'];
 
 	}
 }
 $query='select Material, SupplyDate, MaterialQuantity, JobInd from materialsused where JobInd='.$rowJ['Ind'].'	order by SupplyDate;';
-$MaterialsRes=mysql_query($query);
-if(!$MaterialsRes){
-	die(mysql_error());
-}
-if (mysql_num_rows($MaterialsRes)!=0){
+$MaterialsRes=dbquery($query);
+if (dbnumrows($MaterialsRes)!=0){
 	echo 	'<tr><td class="SubHeading3">Materials<td class="SubHeading3">Date<td class="SubHeading3" colspan="3">Qty';
-	while ($rowM=mysql_fetch_assoc($MaterialsRes)){
+	while ($rowM=dbfetchassoc($MaterialsRes)){
 		echo 	'<tr><td class="general">'.$rowM['Material'].'<td class="general">'.date('j/n/Y',strtotime($rowM['SupplyDate'])).'<td class="general" colspan="3">'.$rowM['MaterialQuantity'];
 
 	}
@@ -166,18 +114,9 @@ if (mysql_num_rows($MaterialsRes)!=0){
 //end of details
 
 }
-echo '<tr><td colspan="5" class="general"><input type="hidden" name="JobQuery" value="'.$queryjobs.'"><input type="hidden" value="'.$_POST['EmployeeInd'].'" name="EmployeeInd"><input type="hidden" name="InvoiceNumber" value="'.$_POST['Invoice'].'"><center><input type="submit" value="Print">';
+echo '<tr><td colspan="5" class="general"><input type="hidden" name="JobQuery" value="'.$queryjobs.'"><input type="hidden" value="'.$_SESSION['EmployeeInd'].'" name="EmployeeInd"><input type="hidden" name="InvoiceNumber" value="'.$_POST['Invoice'].'"><center><input type="submit" value="Print">';
 echo '</table></form>';
 ?>
-
 </div>
-
-
-
-
-
-
-<div id="background">&nbsp</div>
-
 </body>
 </html>
--- ViewServiceHistory.php
+++ ViewServiceHistory.php
@@ -1,48 +1,26 @@
 <?php
-//load global functions, check that user is logged in, and initialise database***********
-session_start();
-include 'GlobalFunctions.php';
-if ($_SESSION['loggedin']!=true){
-die('You are not authorised to view this page');
-}
-if (checktimeout()){
-	die('Your connection has expired please log back in.<a href="ParkerBros.php">Return Home</a>');
-}
+require_once 'GlobalFunctions.php';
+require_once 'data/dbintegration.php';
+
 echo '<html>
       <head>
       <title>Parker Bros Earthmoving Pty Ltd</title>';
 echo MobileDetect(); /*must be in html header*/
-?>
-  
-</head>
-<body>
+echo '</head>
+     <body>
 
-<img id="topbanner" src="images\pbbanner1.jpg"  border="0">
-<div id="topbanner">
-Parker Bros Earthmoving Pty Ltd.
-</div>
+     <img id="topbanner" src="images\pbbanner1.jpg"  border="0">
+     <div id="topbanner">
+     Parker Bros Earthmoving Pty Ltd.
+     </div>
 
-
-<?php
-//connect to database
-$link = @mysql_connect($_SESSION['host'], $_SESSION['user'], $_SESSION['pass']);
-if (!$link) {
-    die('Could not connect to MySQL server: ' . mysql_error());
-}
-$dbname = $_SESSION['datab'];
-$db_selected = mysql_select_db($dbname, $link);
-if (!$db_selected) {
-    die("Could not set $dbname: " . mysql_error());
-}
-
-
+     <div id="background">&nbsp</div>';
 
 StandardMenu();
-if ($_SESSION['loggedin'] and !checktimeout()){
-	LoggedInMenu();
-}
+if (checktimeout()){die('<div id= "scroller">You are not authorised to view this page or your session has expired please log in. <a href="ParkerBros.php">Return Home</a></div>');}
+LoggedInMenu();
 ?>
-//begin page specific code******************************************************
+<div id="background">&nbsp</div>
 <div id="scroller">
 <p class="general">
 <?php
@@ -54,21 +32,15 @@ if($_POST['Vehicle']=='All'){
 $query = 'Select * from parkerbros.servicehistory where Vehicle="'.$_POST['Vehicle'].'" and ServDate between "'.$StartDate.'" and "'.$EndDate.'" order by ServDate DESC;';
 }
 
-$res = mysql_query($query);
-if(!$res){
-	die(mysql_error());
-}
+$res = dbquery($query);
 $query2 = 'select * from parkerbros.Vehicles where Ind="'.$_POST['Vehicle'].'";';
-$res2 = mysql_query($query2);
-if(!$res2){
-	die(mysql_error());
-}
-$row2=mysql_fetch_assoc($res2);
+$res2 = dbquery($query2);
+$row2=dbfetchassoc($res2);
 
 echo '<form action="PrintServiceHistory.php" method="post"><table><tr><td colspan="8" class="SubHeading1">Service History for '.$row2['Make'].' '.$row2['Model'].', '.$row2['Name'].'<tr>'.
      '<td colspan="8" class="general">From '.date('l jS \of F, Y.',strtotime($StartDate)).' to '.date('l jS \of F, Y.',strtotime($EndDate)).
      '<tr><td class="general">Date<td class="general">Amount<td class="general">Units<td class="general">Schedueled<td class="general">DailyChecklist#<td class="general">Item serviced<td class="general">Work Completed  <td class="general">Comments';
-while($row=mysql_fetch_assoc($res)){
+while($row=dbfetchassoc($res)){
      echo '<tr><td class="general">'.$row['ServDate'].'<td class="general">'.$row['ServUnitsAmount'].'<td class="general">'.$row['ServUnitsType'].'<td class="general">'.$row['SchedueledService'].'<td class="general">'.$row['DailyChecklistInd'].'<td class="general">'.$row['ServItem'].'<td class="general">'.$row['WorkCompleted'].'<td class="general">'.$row['Comments'];
 
 
@@ -83,9 +55,5 @@ echo '<input name="StartDate" type="hidden" value="'.$StartDate.'"><input name="
 ?>
 </p>
 </div>
-
-//end page specific code********************************************************
-<div id="background">&nbsp</div>
-
 </body>
 </html>
