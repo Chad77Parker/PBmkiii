e8340f4162da816c613e1a31750e86b66cd0665e
Chad77Parker <32125713+Chad77Parker@users.noreply.github.com>
2018-09-10 17:42:30 +1000

    Add files via upload

--- PBIndexList.php
+++ PBIndexList.php
@@ -1,51 +1,42 @@
 <?php
-session_start();
-include 'GlobalFunctions.php';
-if ($_SESSION['loggedin']!=true){
-die('You are not authorised to view this page');
-}
-if (checktimeout()){
-	die('Your connection has expired please log back in.<a href="ParkerBros.php">Return Home</a>');
-}
+require_once 'GlobalFunctions.php';
+require_once 'data/dbintegration.php';
+
 echo '<html>
       <head>
       <title>Parker Bros Earthmoving Pty Ltd</title>';
-echo MobileDetect();
+echo MobileDetect(); /*must be in html header*/
+echo '</head>
+     <body>
+
+     <img id="topbanner" src="images\pbbanner1.jpg"  border="0">
+     <div id="topbanner">
+     Parker Bros Earthmoving Pty Ltd.
+     </div>
+
+     <div id="background">&nbsp</div>';
+
+
+if (checktimeout()){die('<div id= "scroller">You are not authorised to view this page or your session has expired please log in. <a href="ParkerBros.php">Return Home</a></div>');}
+
 ?>
-  
-</head>
-<body>
+<div id="background">&nbsp</div>
 <div id="scroller">
 <?php
-//connect to database
-$link = @mysql_connect($_SESSION['host'], $_SESSION['user'], $_SESSION['pass']);
-if (!$link) {
-    die('Could not connect to MySQL server: ' . mysql_error());
-}
-$dbname = $_SESSION['datab'];
-$db_selected = mysql_select_db($dbname, $link);
-if (!$db_selected) {
-    die("Could not set $dbname: " . mysql_error());
-}
-
-
 echo '<table><tr><td class="SubHeading3">PBIndex<td class="SubHeading3">Part Number<td 	class="SubHeading3">Vehicle<td class="SubHeading3">Description';
-$query='select * from parkerbros.SpareParts;';
-$res=mysql_query($query);
-if (!$res){
-	die(mysql_error());
-}
-while ($row = mysql_fetch_assoc($res)) {
+$query='select spareparts.*, concat(vehicles.Make," ",vehicles.Model,", ",vehicles.Name," REG:",vehicles.Registration) as V from spareparts left outer join Vehicles on Vehicle=vehicles.Ind;';
+$res=dbquery($query);
+while ($row = dbfetchassoc($res)) {
 	echo '<tr><td class="general">'.$row['PBIndex'].'
 		<td class="general">'.$row['PartNumber'].'
-		<td class="general">'.$row['Vehicle'].'
+		<td class="general">'.$row['V'].'
 		<td class="general">'.$row['Description'];
 }
 echo '</table>';
 
 ?>
 </div>
-<div id="background">&nbsp</div>
+
 
 
 </body>
